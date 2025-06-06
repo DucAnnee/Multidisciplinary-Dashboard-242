75c5c9f738cc259686824c419bd60672b26e89d0
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-06-06 07:29:44 +0700

    feat: accomodate sidebar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,28 @@
 import Appbar from "./components/Appbar";
-import Footer from "./components/Footer";
 import { Outlet, useLocation } from "react-router-dom";
 
 function App() {
-  const location = useLocation();
-  const isGuestRoute = location.pathname.startsWith("/guest");
-
   return (
-    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <Appbar />
-      <div style={{ alignSelf: "center", flex: 1 }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Appbar sx={{ width: "12%" }} />
+      <div
+        style={{
+          alignSelf: "center",
+          flex: 1,
+          backgroundColor: "#E3F0E5",
+          minHeight: "100vh",
+          width: "86%",
+        }}
+      >
         <Outlet />
       </div>
-      <Footer />
     </div>
   );
 }
